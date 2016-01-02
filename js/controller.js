9c3028cd634e025852134261604012ff4267233d
Joe Neuman <Joe Neuman>
2016-01-01 20:43:21 -0700

    1 - 5 done

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,8 @@
 angular.module('myApp').controller('mainControl',
     function ($scope, myService) {
         $scope.name = myService.saySomething;
-
-
-
+        $scope.living = myService.living;
+      
+        $scope.getUsers = myService.getUsers;
 
     });
--- js/service.js
+++ js/service.js
@@ -1,3 +1,14 @@
 angular.module('myApp').service('myService', function () {
+    
+    
     this.saySomething = "Controller.js, App.js and Service.js Conected";
+    this.living = ['Monkey', 'Dog', 'Cat', 'Mouse'];
+    
+    var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://smurfs.devmounta.in/smurfs/", false);
+xhr.send();
+
+document.getElementById("log1").innerHTML=(xhr.status);
+document.getElementById("log2").innerHTML=(xhr.statusText);
+    
 });
